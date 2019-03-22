0cca46390dc2d849f9b5fd126564abc86fd001ea
Kiddo <41273310+SatoshiKiddo@users.noreply.github.com>
2019-03-22 09:35:12 -0400

    Interfaz de respuestas

---
+++ ServicioLotoUCAB.Servicio.Servicio/Login/Utilidades/Respuesta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServicioLotoUCAB.Servicio.Servicio.Utilidades
+{
+    /// <summary>
+    /// Class <c>Respuesta</c>
+    /// Conforma la respuesta estándar otorgada por el usuario.
+    /// </summary>
+    [DataContract]
+    public class Respuesta
+    {
+        /// <summary>
+        /// Establece el status de respuesta, siendo el código del mismo.
+        /// </summary>
+        protected int _statusRespuesta;
+        /// <summary>
+        /// Establece la descripción de la respuesta, logrando establecer cualquier tipo de acción dentro del servicio.
+        /// </summary>
+        protected string _descripcion;
+        /// <summary>
+        /// Establece el error atrapada dentro de la ejecución del servicio.
+        /// </summary>
+        protected string _error;
+
+        [DataMember]
+        public int StatusResp
+        {
+            get { return _statusRespuesta; }
+            set { _statusRespuesta = value; }
+        }
+
+        [DataMember]
+        public string Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+        [DataMember]
+        public string Error
+        {
+            get { return _error; }
+            set { _error = value; }
+        }
+
+    }
+}
---
+++ ServicioLotoUCAB.Servicio.Servicio/Login/Utilidades/RespuestaDashboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServicioLotoUCAB.Servicio.Comunes;
+using System.Runtime.Serialization;
+
+namespace ServicioLotoUCAB.Servicio.Servicio.Utilidades
+{
+    /// <summary>
+    /// Class <c>RespuestaDashboard</c>
+    /// Conforma la respuesta del dashboard luego de realizar el ingreso dentro del´sistema.
+    /// </summary>
+    public class RespuestaDashboard: Respuesta
+    {
+        /// <summary>
+        /// Establece el tablero con la información necesaria para proceder y ejecutar los servicios dentro del sistema.
+        /// </summary>
+        private Dashboard _tablero;
+
+        [DataMember]
+        public Dashboard Tablero
+        {
+            get
+            { return _tablero; }
+            set
+            { _tablero = value; }
+        }
+    }
+}
